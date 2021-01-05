99e7137f2d6144cff45c2449076028d378d77d07
Sébastien <seb.lasante@hotmail.com>
2021-01-05 11:27:30 +0100

    fini

---
+++ public/js/main.js
@@ -0,0 +1,74 @@
+//  let a = 1
+//  let b = 2
+//  let c = 3
+
+
+//  let temp = a
+//    a = b
+//    b = c
+//    c = temp
+// console.log(a,b,c)
+
+// EXO1
+// - ## Afficher 4, 3, 2, 1
+
+// let a = 1
+// let b = 2
+// let c = 3
+// let d = 4
+
+// temp1=b
+// temp2=a
+
+// a=d
+// b=c
+// c=temp1
+// d=temp2
+// console.log(a,b,c,d);
+
+// Exo2
+// 2341
+// let a = 1
+// let b = 2
+// let c = 3
+// let d = 4
+// temp1=a
+
+// a=b
+// b=c
+// c=d
+// d=temp1
+// console.log(a,b,c,d);
+
+// EXO3
+// let a= 4
+// let b= 3
+// let c= 2
+// let d= 1
+
+// let tableau = [a, b, c, d];
+// console.log(tableau.reverse());
+
+// EXO4
+// 451263
+
+
+let a=1
+let b=2
+let c=3
+let d=4
+let e=5
+let f=6
+
+temp1=a
+temp2=b
+temp3=c
+
+a = d
+b = e
+c = temp1
+d = temp2
+e = f
+f = temp3
+
+console.log(a,b,c,d,e,f);
